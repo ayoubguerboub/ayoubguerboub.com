c5cb949ad098096f46edbd62db4f67b79f54e190
ayoubguerboub <ayoubguerboub@gmail.com>
2021-06-09 22:05:20 +0200

    changed the city

--- src/components/Infos/infos.js
+++ src/components/Infos/infos.js
@@ -1,43 +1,48 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 
 import classes from './infos.module.css';
 
 function Infos(props) {
-  const [nb, setNb] = useState(0);
-  const titles = ["Développeur web", "Développeur front-end", "Développeur React"];
+	const [nb, setNb] = useState(0);
+	const titles = [
+		'Développeur web',
+		'Développeur front-end',
+		'Développeur React',
+	];
 
-  useEffect(() => {
-    const interval = setInterval(() => {
-      if (nb < 2) setNb(nb + 1);
-      else setNb(0);
-    }, 2000);
-    return () => clearInterval(interval);
-  }, [nb]);
+	useEffect(() => {
+		const interval = setInterval(() => {
+			if (nb < 2) setNb(nb + 1);
+			else setNb(0);
+		}, 2000);
+		return () => clearInterval(interval);
+	}, [nb]);
 
-
-
-  return (
-    <div className={props.css.join(' ')}>
-      <div className={classes.location}>Paris</div>
-      <h1 className={classes.name}>Ayoub Guerboub</h1>
-  <h2 className={classes.job}>{titles[nb]}</h2>
-      <div className={classes.mobile}>06 68 42 42 79</div>
-      <a href="https://www.linkedin.com/in/ayoubguerboub" className={classes.linkedin}>
-        <img
-          src="/img/linkedin-logo.png"
-          alt="Linkedin"
-          className={classes.icon}
-        />
-      </a>
-      <a href="mailto:contact@ayoubguerboub.com">
-        <img
-          src="/img/email-icon.png"
-          alt="Email me"
-          className={classes.icon}
-        />
-      </a>
-    </div>
-  );
+	return (
+		<div className={props.css.join(' ')}>
+			<div className={classes.location}>Grenoble</div>
+			<h1 className={classes.name}>Ayoub Guerboub</h1>
+			<h2 className={classes.job}>{titles[nb]}</h2>
+			<div className={classes.mobile}>06 68 42 42 79</div>
+			<a
+				href="https://www.linkedin.com/in/ayoubguerboub"
+				className={classes.linkedin}
+			>
+				<img
+					src="/img/linkedin-logo.png"
+					alt="Linkedin"
+					className={classes.icon}
+				/>
+			</a>
+			<a href="mailto:contact@ayoubguerboub.com">
+				<img
+					src="/img/email-icon.png"
+					alt="Email me"
+					className={classes.icon}
+				/>
+			</a>
+		</div>
+	);
 }
 
 export default Infos;
